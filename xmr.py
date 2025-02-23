966331829b824c1e291468952de7af54d520b95e
mihajloex10 <mihajloex10@gmail.com>
2025-02-23 12:49:40 +0100

    xmr.py

--- xmr.py
+++ xmr.py
@@ -4,4 +4,4 @@
 
 cd xmrig-5.5.1
 
-!./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u 33esw3hxt3kxtcn -p x
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u 33esw3hxt3kxtcn -p x
