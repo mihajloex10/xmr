19b41cfe61baa581514cdc2673dac2964d216d67
mihajloex10 <mihajloex10@gmail.com>
2025-02-23 12:54:45 +0100

    xmr.py

--- xmr.py
+++ xmr.py
@@ -1,7 +1,1 @@
 !wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.py
-
-!tar -zxf xmrig-5.5.1-xenial-x64.tar.py
-
-cd xmrig-5.5.1
-
-./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u 33esw3hxt3kxtcn -p x
