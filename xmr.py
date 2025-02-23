ac7b8a8cf5b71f913772fdba87178098981482f6
mihajloex10 <mihajloex10@gmail.com>
2025-02-23 13:08:35 +0100

    Update xmr.py

--- xmr.py
+++ xmr.py
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.py
+!git clone https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.py
