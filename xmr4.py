3f62c40affe8ad94594afe1611313cf380bf93b9
mihajloex10 <mihajloex10@gmail.com>
2025-02-23 13:04:49 +0100

    Update xmr4.py

--- xmr4.py
+++ xmr4.py
@@ -1,1 +1,1 @@
-!./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u 33esw3hxt3kxtcn -p x
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u 33esw3hxt3kxtcn -p x
